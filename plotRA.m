3c7cc42d13fee73b1efe83f26491e5b3bceb6f92
Zhen Fu <fuzhenfz@126.com>
2019-04-13 21:18:49 +0800

    Add files via upload

---
+++ plotRA.m
@@ -0,0 +1,45 @@
+function RA = plotRA(signal,fs,freRange)
+Nfft = fs;
+f_signal = abs(fft(signal,Nfft));% 将信号补零直至长度为Nfft个点，此时计算FFT分辨率为1 Hz
+f_signal = f_signal(1:Nfft/2);
+f_signal = [f_signal(2:end) 0];
+% 给频谱求相对频谱
+width = 500;
+inside = 5;
+a = floor(inside/2);
+n = length(f_signal);
+RA = zeros(n,1);
+
+if width >= n/2
+    disp('Too long to make!');
+    return;
+else
+    for i = 1:n
+        if i <= width
+            RA(i) = f_signal(i)/sum(f_signal(i+1:i+width))*width;
+        elseif i >= n - width
+            RA(i) = f_signal(i)/sum(f_signal(i-width:i-1))*width;
+        else
+            RA(i) = (sum(f_signal(i-a:i+a))/inside)/...
+                ((sum(f_signal(i-width:i-a-1))+sum(f_signal(i+a+1:i+width)))/(2*width-inside));
+        end
+    end    
+end
+
+if strcmp(class(RA),'double')==0
+    RA = double(RA);
+end
+% RA = 20*log10(RA);
+for i2=1:length(signal)
+    if f_signal(i2)<1e-2
+        RA(i2)=0;
+    end
+end
+if nargout==0
+    % figure;
+    plot(RA);
+    xlabel('Frequency (Hz)','fontsize',12)
+    ylabel('Relative Amplitude (dB)','fontsize',12)
+    xlim(freRange)
+end
+
